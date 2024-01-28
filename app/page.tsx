8b44e202514f78c0a74957377bca58ca67e22bcd
Fahmi Fauzi <fahmi.fauzi@ifg-life.id>
2024-01-28 09:41:08 +0700

    update aout

--- app/page.tsx
+++ app/page.tsx
@@ -81,15 +81,12 @@ const Home = () => {
       </section>
 
       <div data-aos="fade-up">
-        <section id='about' className='mt-12 bg-[#F1F5FB] w-full px-[8.1%] flex flex-row items-center gap-x-7 py-10 lg:flex-col lg:gap-x-0 lg:gap-y-7'>
-          <div className="relative w-[40%] lg:w-full lg:flex lg:justify-center">
-            {/* <img src="/images/kaneki.jpeg" alt="kaneki" className='lg:w-full shadow-md h-[270px] object-cover' /> */}
-            {/* <Blob2 className="mr-0 lg:-mr-5" /> */}
-              <Blob2 className='sm:w-[360px] sm:h-[310px] sm:ml-3' />
+        <section id='about' className='w-full mt-12 bg-[#f1f5fb] py-10 px-[8.1%] flex items-center lg:flex-col'>
+          <div className="">
+            <Blob2 className='w-[470px] h-[470px] sm:w-[300px] sm:h-[300px] sm:-mr-3' />
           </div>
 
-
-          <div className="w-[60%] lg:w-full">
+          <div className="">
             <span className='relative w-fit text-xl font-semibold text-primary'>Let me introduce myself!
               <span className='absolute w-full h-[5px] bg-sky-500 opacity-30 left-0 right-0 bottom-0 mb-1'></span>
             </span>
